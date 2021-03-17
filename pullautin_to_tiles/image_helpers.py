cf8874dc7117c8a580bd41a0e651539bc44c7a3c
Nicolas Rio <nicolas.rio42@gmail.com>
2021-03-17 19:13:06 +0100

    Tiling machine is working

---
+++ pullautin_to_tiles/image_helpers.py
@@ -0,0 +1,90 @@
+from PIL import Image
+
+def fill_small_png_with_alpha(png_path, pgw_path, tile_pixel_size, tile_meter_size, tile_geometry):
+    with open(pgw_path, "r") as f:
+        pgw_file = f.read()
+    splited_pgw_file = pgw_file.splitlines()
+    # png are georeferenced with the up left point
+    png_georef_point = [float(splited_pgw_file[4]), float(splited_pgw_file[5])]
+    
+    # Tile scale in meters per pixels
+    tile_scale = tile_meter_size / tile_pixel_size
+    png = Image.open(png_path)
+    image_width, image_height = png.size
+
+    png_right_down_point = (
+        png_georef_point[0] + image_width * tile_scale,
+        png_georef_point[1] - image_height * tile_scale
+    )
+
+    fill_left = False
+    fill_right = False
+    fill_up = False
+    fill_down = False
+
+    if image_width < tile_pixel_size:
+        if abs(png_georef_point[0] - tile_geometry[0][0]) > 1:
+            fill_left = True
+        if abs(png_right_down_point[0] - tile_geometry[2][0]) > 1:
+            fill_right = True
+    if image_height < tile_pixel_size:
+        if abs(png_georef_point[1] - tile_geometry[0][1]) > 1:
+            fill_up = True
+        if abs(png_right_down_point[1] - tile_geometry[2][1]) > 1:
+            fill_down = True
+
+    if fill_left and fill_right:
+        left_transparent_width = int(abs(png_georef_point[0] - tile_geometry[0][0]) / tile_scale)
+        right_transparent_width = tile_pixel_size - image_width - left_transparent_width
+        left_transparent = create_transparent_image(left_transparent_width, image_height)
+        right_transparent = create_transparent_image(right_transparent_width, image_height)
+        merged_left = get_concat_h(left_transparent, png)
+        merged_h = get_concat_h(merged_left, right_transparent)
+    elif fill_left and not fill_right:
+        left_transparent_width = tile_pixel_size - image_width
+        left_transparent = create_transparent_image(left_transparent_width, image_height)
+        merged_h = get_concat_h(left_transparent, png)
+    elif not fill_left and fill_right:
+        right_transparent_width = tile_pixel_size - image_width
+        right_transparent = create_transparent_image(right_transparent_width, image_height)
+        merged_h = get_concat_h(png, right_transparent)
+
+    else:
+        merged_h = png
+    
+    if fill_up and fill_down:
+        up_transparent_height = int(abs(png_georef_point[1] - tile_geometry[0][1]) / tile_scale)
+        down_transparent_height = tile_pixel_size - image_height - up_transparent_height
+        up_transparent = create_transparent_image(tile_pixel_size, up_transparent_height)
+        down_transparent = create_transparent_image(tile_pixel_size, down_transparent_height)
+        merged_up = get_concat_v(up_transparent, merged_h)
+        merged = get_concat_v(merged_up, down_transparent)
+    elif fill_up and not fill_down:
+        up_transparent_height = tile_pixel_size - image_height
+        up_transparent = create_transparent_image(tile_pixel_size, up_transparent_height)
+        merged = get_concat_v(up_transparent, merged_h)
+    elif not fill_up and fill_down:
+        down_transparent_height = tile_pixel_size - image_height
+        down_transparent = create_transparent_image(tile_pixel_size, down_transparent_height)
+        merged = get_concat_v(merged_h, down_transparent)
+    else:
+        merged = merged_h
+
+    return merged
+    
+def get_concat_h(im1, im2):
+    dst = Image.new('RGBA', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+def get_concat_v(im1, im2):
+    dst = Image.new('RGBA', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
+def create_transparent_image(width, height):
+    img = Image.new('RGBA', (width, height), (255, 0, 0, 0))
+    return img
+    
--- pullautin_to_tiles/lambert_93.py
+++ pullautin_to_tiles/lambert_93.py
@@ -3,7 +3,16 @@ import math
 import os
 from shutil import copyfile
 
+from PIL import Image
+
 import helpers as hp
+import image_helpers as im_hp
+
+TILES_PIXEL_SIZE = 1183
+ZOOM_NUMBER = 12
+(MIN_X, MAX_X, MIN_Y, MAX_Y) = (-343646, 1704354, 5619537, 7667537)
+MAX_TILE_SIZE = 500 * math.pow(2, 12)
+SLIPPY_MAP_ORIGIN = (MIN_X, MAX_Y)
 
 def zoom_number(minimum_tile_size, border_size):
     """Return the number of zooms
@@ -21,8 +30,7 @@ def lambert_93_to_tile_num(x, y, origin, max_size, zoom):
     y_tile = int(((origin[1] - y) / max_size) * math.pow(2, zoom))
     return (x_tile, y_tile)
 
-
-if __name__ == "__main__":
+def slippy_map_parameters():
     france_border = hp.load_geojson_file("lambert_93_index_files\\france_border.geojson")
     france_extrema = hp.polygon_extrema(france_border)
     france_size = max_height_or_width(france_extrema)
@@ -49,6 +57,7 @@ if __name__ == "__main__":
     print(tile_exemple)
     print(tile_exemple_2)
 
+def make_max_zoom_tiles():
     lidar_index_lambert_93 = hp.load_geojson_file("lambert_93_index_files\\lidar_index_lambert_93.geojson")
     try:
         png_files = [f for f in os.listdir("in") if f.endswith(".png")]
@@ -65,7 +74,78 @@ if __name__ == "__main__":
         tile_number = int(png[30:36])
         tile = [t for t in lidar_index_lambert_93["features"] if t["properties"]["TILES_500m"] == tile_number]
         point = tile[0]["geometry"]["coordinates"][0][0][0]
-        tile_coord = lambert_93_to_tile_num(point[0], point[1], slipy_map_origin, max_tile_size, 12)
+        tile_coord = lambert_93_to_tile_num(point[0], point[1], SLIPPY_MAP_ORIGIN, MAX_TILE_SIZE, 12)
         hp.make_dir_if_doesnt_exist("tiles\\12\\" + str(tile_coord[0]))
-        copyfile(("in\\" + png), ("tiles\\12\\" + str(tile_coord[0]) + "\\" + str(tile_coord[1]) + ".png"))
-        
+        png_path = os.path.join("in", png)
+        png_file = Image.open(png_path)
+        png_width, png_height = png_file.size
+        # print(tile_number)
+        # print(tile_coord)
+
+        if png_width == TILES_PIXEL_SIZE and png_height == TILES_PIXEL_SIZE:
+            copyfile(png_path, ("tiles\\12\\" + str(tile_coord[0]) + "\\" + str(tile_coord[1]) + ".png"))
+        else:
+            pgw_path = os.path.splitext(png_path)[0] + ".pgw"
+            tile_geometry = tile[0]["geometry"]["coordinates"][0][0]
+            filled_png = im_hp.fill_small_png_with_alpha(png_path, pgw_path, TILES_PIXEL_SIZE, 500, tile_geometry)
+            filled_png.save(("tiles\\12\\" + str(tile_coord[0]) + "\\" + str(tile_coord[1]) + ".png"), 'PNG')
+
+def get_parent_tile(x_tile, y_tile):
+    x_parent = int(x_tile / 2)
+    y_parent = int(y_tile / 2)
+    return (x_parent, y_parent)
+
+def get_children_tiles(x_tile, y_tile):
+    x_children = (x_tile * 2, x_tile * 2 + 1)
+    y_children = (y_tile * 2, y_tile * 2 + 1)
+    return (x_children, y_children)
+
+def make_tiles(zoom):
+    parent_tiles_path = os.path.join("tiles", str(zoom))
+    hp.make_dir_if_doesnt_exist(parent_tiles_path)
+    children_tiles_path = os.path.join("tiles", (str(zoom + 1)))
+    x_list = os.listdir(children_tiles_path)
+    for x in x_list:
+        x_path = os.path.join(children_tiles_path, str(x))
+        y_list = os.listdir(x_path)
+        for y in y_list:
+            x_parent, y_parent = get_parent_tile(int(x), int(os.path.splitext(y)[0]))
+            parent_path = os.path.join(parent_tiles_path, str(x_parent), str(y_parent) + ".png")
+            if not os.path.exists(parent_path):
+                hp.make_dir_if_doesnt_exist(os.path.join(parent_tiles_path, str(x_parent)))
+                children_tiles = get_children_tiles(x_parent, y_parent)
+                x0_y0_path = os.path.join(children_tiles_path, str(children_tiles[0][0]), str(children_tiles[1][0]) + ".png")
+                x1_y0_path = os.path.join(children_tiles_path, str(children_tiles[0][1]), str(children_tiles[1][0]) + ".png")
+                x0_y1_path = os.path.join(children_tiles_path, str(children_tiles[0][0]), str(children_tiles[1][1]) + ".png")
+                x1_y1_path = os.path.join(children_tiles_path, str(children_tiles[0][1]), str(children_tiles[1][1]) + ".png")
+                if os.path.exists(x0_y0_path):
+                    x0_y0 = Image.open(x0_y0_path)
+                else:
+                    x0_y0 = im_hp.create_transparent_image(TILES_PIXEL_SIZE, TILES_PIXEL_SIZE)
+                
+                if os.path.exists(x1_y0_path):
+                    x1_y0 = Image.open(x1_y0_path)
+                else:
+                    x1_y0 = im_hp.create_transparent_image(TILES_PIXEL_SIZE, TILES_PIXEL_SIZE)
+                
+                if os.path.exists(x0_y1_path):
+                    x0_y1 = Image.open(x0_y1_path)
+                else:
+                    x0_y1 = im_hp.create_transparent_image(TILES_PIXEL_SIZE, TILES_PIXEL_SIZE)
+                
+                if os.path.exists(x1_y1_path):
+                    x1_y1 = Image.open(x1_y1_path)
+                else:
+                    x1_y1 = im_hp.create_transparent_image(TILES_PIXEL_SIZE, TILES_PIXEL_SIZE)
+                
+                merged_up = im_hp.get_concat_h(x0_y0, x1_y0)
+                merged_down = im_hp.get_concat_h(x0_y1, x1_y1)
+                merged = im_hp.get_concat_v(merged_up, merged_down)
+                merged_resized = merged.resize((TILES_PIXEL_SIZE, TILES_PIXEL_SIZE))
+                merged_resized.save(parent_path, 'PNG')
+
+
+if __name__ == "__main__":
+    zoom_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    for zoom in zoom_list:
+        make_tiles(zoom)
